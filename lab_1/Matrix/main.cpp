a4b7a556b3f1fc4747dd2bf933c1dcba76b404b4
dmitrii <VoronindimTvink123@mail.ru>
2020-02-28 19:31:50 +0300

    Добавлен вывод с окргулением до 3 знака.

--- lab_1/Matrix/main.cpp
+++ lab_1/Matrix/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <cmath>
 
 using namespace std;
 const int matrixSize = 3;
@@ -9,13 +10,13 @@ int checkOpen(ifstream& file)
     file.open("matrix.txt");
     if (!(file.is_open()))
     {
-	cout << "NO FILE" << "\n";
-	return 1;
+	    cout << "NO FILE" << "\n";
+	    return 1;
     }
     return 0;
 }
 
-void FillingMatrix(double (&matrix)[matrixSize][matrixSize],  ifstream& inputMatrix)
+void FillingMatrix(double (&matrix)[matrixSize][matrixSize],  istream& inputMatrix)
 {
     for (int i = 0; i < matrixSize;i++)
     {
@@ -26,7 +27,7 @@ void FillingMatrix(double (&matrix)[matrixSize][matrixSize],  ifstream& inputMat
     }
 }
 
-double CountDeterminant(double (&matrix)[matrixSize][matrixSize], double determinant)
+void CountDeterminant(double (&matrix)[matrixSize][matrixSize], double& determinant)
 {
     determinant = ((matrix[0][0] * matrix[1][1] * matrix[2][2])
                 + (matrix[1][0] * matrix[0][2] * matrix[2][1])
@@ -41,17 +42,26 @@ double FillingMinorMatrix(double (&matrix)[matrixSize][matrixSize], double (&min
     minorMatrix[0][0] = matrix[1][1] * matrix[2][2] - matrix[2][1] * matrix[1][2];
     minorMatrix[0][1] = (-1) * (matrix[1][0] * matrix[2][2] - matrix[2][0] * matrix[1][2]);
     minorMatrix[0][2] = matrix[1][0] * matrix[2][1] - matrix[2][0] * matrix[1][1];
+
     minorMatrix[1][0] = (-1) * (matrix[0][1] * matrix[2][2] - matrix[2][1] * matrix[0][2]);
     minorMatrix[1][1] = matrix[0][0] * matrix[2][2] - matrix[2][0] * matrix[0][2];
     minorMatrix[1][2] = (-1) * (matrix[0][0] * matrix[2][1] - matrix[2][0] * matrix[0][1]);
+
     minorMatrix[2][0] = matrix[0][1] * matrix[1][2] - matrix[1][1] * matrix[0][2];
     minorMatrix[2][1] = (-1) * (matrix[0][0] * matrix[1][2] - matrix[1][0] * matrix[0][2]);
     minorMatrix[2][2] = matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1];
-    for (int i = 0; i < 3 ; ++i) {
-        for (int j = 0; j < 3 ; ++j) {
-            cout << minorMatrix[i][j] << " ";
+}
+
+void OutputResult(double(&minorMatrix)[matrixSize][matrixSize], const double& determinant)
+{
+    for (int i = 0; i < matrixSize ; i++)
+    {
+        for (int j = 0; j < matrixSize; j++)
+        {
+            cout << round((minorMatrix[i][j] / determinant) * 1000) / 1000 << " ";
+
         }
-        cout << endl;
+        cout << "\n";
     }
 }
 
@@ -70,17 +80,8 @@ int main()
     }
     double minorMatrix[matrixSize][matrixSize];
     FillingMinorMatrix(matrix, minorMatrix);
-
+    OutputResult(minorMatrix, determinant);
 
     return 0;
 }
-/*
-for (int i = 0; i < size;i++)
-{
-  for (int j = 0; j < size; j++)
-  {
-    cout << transportedMatrix[i][j] << " ";
-  }
-  cout << endl;
-}
-*/
+
