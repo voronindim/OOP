2b8eac1714f77bffa3000b1cae32802f54da0b10
dmitrii <VoronindimTvink123@mail.ru>
2020-02-28 18:40:19 +0300

    Программа без вывода!Создание матрицы миноров, изменение знаков у некоторых элеиентов матрицы. Подсчет определителя.

---
+++ lab_1/Matrix/main.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+const int matrixSize = 3;
+
+int checkOpen(ifstream& file)
+{
+    file.open("matrix.txt");
+    if (!(file.is_open()))
+    {
+	cout << "NO FILE" << "\n";
+	return 1;
+    }
+    return 0;
+}
+
+void FillingMatrix(double (&matrix)[matrixSize][matrixSize],  ifstream& inputMatrix)
+{
+    for (int i = 0; i < matrixSize;i++)
+    {
+	    for (int j = 0; j < matrixSize;j++)
+	    {
+	        inputMatrix >> matrix[i][j];
+	    }
+    }
+}
+
+double CountDeterminant(double (&matrix)[matrixSize][matrixSize], double determinant)
+{
+    determinant = ((matrix[0][0] * matrix[1][1] * matrix[2][2])
+                + (matrix[1][0] * matrix[0][2] * matrix[2][1])
+		        + (matrix[2][0] * matrix[0][1] * matrix[1][2]))
+		        - ((matrix[2][0] * matrix[1][1] * matrix[0][2])
+		        + (matrix[1][0] * matrix[0][1] * matrix[2][2])
+		        + (matrix[0][0] * matrix[2][1] * matrix[1][2]));
+}
+
+double FillingMinorMatrix(double (&matrix)[matrixSize][matrixSize], double (&minorMatrix)[matrixSize][matrixSize])
+{
+    minorMatrix[0][0] = matrix[1][1] * matrix[2][2] - matrix[2][1] * matrix[1][2];
+    minorMatrix[0][1] = (-1) * (matrix[1][0] * matrix[2][2] - matrix[2][0] * matrix[1][2]);
+    minorMatrix[0][2] = matrix[1][0] * matrix[2][1] - matrix[2][0] * matrix[1][1];
+    minorMatrix[1][0] = (-1) * (matrix[0][1] * matrix[2][2] - matrix[2][1] * matrix[0][2]);
+    minorMatrix[1][1] = matrix[0][0] * matrix[2][2] - matrix[2][0] * matrix[0][2];
+    minorMatrix[1][2] = (-1) * (matrix[0][0] * matrix[2][1] - matrix[2][0] * matrix[0][1]);
+    minorMatrix[2][0] = matrix[0][1] * matrix[1][2] - matrix[1][1] * matrix[0][2];
+    minorMatrix[2][1] = (-1) * (matrix[0][0] * matrix[1][2] - matrix[1][0] * matrix[0][2]);
+    minorMatrix[2][2] = matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1];
+    for (int i = 0; i < 3 ; ++i) {
+        for (int j = 0; j < 3 ; ++j) {
+            cout << minorMatrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    ifstream inputMatrix;
+    checkOpen(inputMatrix);
+    double matrix[matrixSize][matrixSize];
+    double determinant = 0;
+    FillingMatrix(matrix, inputMatrix);
+    CountDeterminant(matrix, determinant);
+    if (determinant == 0)
+    {
+        cout << "Определитель матрицы равен 0. Такая матрица не имеет обратной! \n";
+        return 1;
+    }
+    double minorMatrix[matrixSize][matrixSize];
+    FillingMinorMatrix(matrix, minorMatrix);
+
+
+    return 0;
+}
+/*
+for (int i = 0; i < size;i++)
+{
+  for (int j = 0; j < size; j++)
+  {
+    cout << transportedMatrix[i][j] << " ";
+  }
+  cout << endl;
+}
+*/
