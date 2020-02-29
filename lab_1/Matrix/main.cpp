f69dff848838308ba905fa2568bb24b3ae9b58ac
dmitrii <VoronindimTvink123@mail.ru>
2020-02-29 20:07:30 +0300

    форматирование

--- lab_1/Matrix/main.cpp
+++ lab_1/Matrix/main.cpp
@@ -103,13 +103,13 @@ bool CreateMatrix(const string& inputFileName, double (&matrix)[matrixSize][matr
 
 double CountDeterminant(double (&matrix)[matrixSize][matrixSize])
 {
-    double determinant;
-	determinant = ((matrix[0][0] * matrix[1][1] * matrix[2][2])
-					  + (matrix[1][0] * matrix[0][2] * matrix[2][1])
-					  + (matrix[2][0] * matrix[0][1] * matrix[1][2]))
-		- ((matrix[2][0] * matrix[1][1] * matrix[0][2])
-			+ (matrix[1][0] * matrix[0][1] * matrix[2][2])
-			+ (matrix[0][0] * matrix[2][1] * matrix[1][2]));
+	double determinant;
+	determinant = matrix[0][0] * matrix[1][1] * matrix[2][2]
+		+ matrix[1][0] * matrix[0][2] * matrix[2][1]
+		+ matrix[2][0] * matrix[0][1] * matrix[1][2]
+		- matrix[2][0] * matrix[1][1] * matrix[0][2]
+		- matrix[1][0] * matrix[0][1] * matrix[2][2]
+		- matrix[0][0] * matrix[2][1] * matrix[1][2];
 	return determinant;
 }
 
