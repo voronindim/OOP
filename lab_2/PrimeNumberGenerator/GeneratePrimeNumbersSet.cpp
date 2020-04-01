f8d3e9b38a229c4b6343f08bc7976cbfa97c11e9
dmitrii <VoronindimTvink123@mail.ru>
2020-04-01 20:27:23 +0300

    простые числа, решето Аткина!

--- lab_2/PrimeNumberGenerator/GeneratePrimeNumbersSet.cpp
+++ lab_2/PrimeNumberGenerator/GeneratePrimeNumbersSet.cpp
@@ -1,36 +1,44 @@
 #include "GeneratePrimeNumbersSet.h"
 
+void GetPrimesByQuadraticForms(vector<bool> &sieveAtkina)
+{
+    int sqrtUpperBound = int(sqrt(sieveAtkina.size() - 1));
+
+    for (int x = 1; x <= sqrtUpperBound; x++)
+    {
+        for (int y = 1; y <= sqrtUpperBound; y++)
+        {
+            int result;
+            result = ((4 * x * x) + (y * y));
+            if (result <= sieveAtkina.size() - 1 && (result % 12 == 1 || result % 12 == 5))
+            {
+                sieveAtkina[result] = !sieveAtkina[result];
+            }
+            result = (3 * x * x) + (y * y);
+            if (result <= sieveAtkina.size() - 1 && (result % 12 == 7))
+            {
+                sieveAtkina[result] = !sieveAtkina[result];
+            }
+            if (x > y)
+            {
+                result = ((3 * x * x) - (y * y));
+                if (result <= sieveAtkina.size() - 1 && result % 12 == 11)
+                {
+                    sieveAtkina[result] = !sieveAtkina[result];
+                }
+            }
+        }
+    }
+}
+
 vector<bool> SearchPrimeNumbers(int upperBound)
 {
 	vector<bool> sieveAtkina(upperBound + 1, false);
 
 	int sqrtUpperBound = int(sqrt(upperBound));
 
-	for (int x = 1; x <= sqrtUpperBound; x++)
-	{
-		for (int y = 1; y <= sqrtUpperBound; y++)
-		{
-			int result;
-			result = ((4 * x * x) + (y * y));
-			if (result <= upperBound && (result % 12 == 1 || result % 12 == 5))
-			{
-				sieveAtkina[result] = !sieveAtkina[result];
-			}
-			result = (3 * x * x) + (y * y);
-			if (result <= upperBound && (result % 12 == 7))
-			{
-				sieveAtkina[result] = !sieveAtkina[result];
-			}
-			if (x > y)
-			{
-				result = ((3 * x * x) - (y * y));
-				if (result <= upperBound && result % 12 == 11)
-				{
-					sieveAtkina[result] = !sieveAtkina[result];
-				}
-			}
-		}
-	}
+    GetPrimesByQuadraticForms(sieveAtkina);
+
 	for (int j = 5; j < sqrtUpperBound; j++)
 	{
 		if (sieveAtkina[j])
