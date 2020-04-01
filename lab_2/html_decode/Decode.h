3e959f9c4a090cd0e550aa37d572f100ba8d48c0
dmitrii <VoronindimTvink123@mail.ru>
2020-04-01 18:26:42 +0300

    delete all files

--- lab_2/html_decode/Decode.h
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include <string>
-#include <sstream>
-
-const char START_SYMBOL_DECODER = '&';
-const char END_SYMBOL_DECODER = ';';
-std::string HtmlDecode(std::string const& html);
