a3d1aa7d2b3da6d4868947fb769e3e6c69aa34e9
dmitrii <VoronindimTvink123@mail.ru>
2020-04-01 18:14:13 +0300

    простые числа, решето Аткина!

---
+++ lab_2/PrimeNumberGenerator/main.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <ctime>
+
+using namespace std;
+
+const int  MAX_UPPER_BOUND = 100000001;
+
+vector<bool> SearchPrimeNumbers(int upperBound)
+{
+	vector<bool> sieveAtkina(upperBound + 1, false);
+
+	int sqrtUpperBound = int(sqrt(upperBound));
+
+	for (int x = 1; x <= sqrtUpperBound; x++)
+	{
+		for (int y = 1; y <= sqrtUpperBound; y++)
+		{
+			int result;
+			result = ((4 * x * x) + (y * y));
+			if (result <= upperBound && (result % 12 == 1 || result % 12 == 5))
+			{
+				sieveAtkina[result] = !sieveAtkina[result];
+			}
+			result = (3 * x * x) + (y * y);
+			if (result <= upperBound && (result % 12 == 7))
+			{
+				sieveAtkina[result] = !sieveAtkina[result];
+			}
+			if (x > y)
+			{
+				result = ((3 * x * x) - (y * y));
+				if (result <= upperBound && result % 12 == 11)
+				{
+					sieveAtkina[result] = !sieveAtkina[result];
+				}
+			}
+		}
+	}
+	for (int j = 5; j < sqrtUpperBound; j++)
+	{
+		if (sieveAtkina[j])
+		{
+			int n = j * j;
+			for (int i = n; i <= upperBound;i += n)
+			{
+				sieveAtkina[i] = false;
+			}
+		}
+	}
+	return sieveAtkina;
+}
+
+set<int> GeneratePrimeNumbersSet(int upperBound)
+{
+	set<int> PrimeNumbers = {2 ,3};
+	vector<bool> sieve = SearchPrimeNumbers(upperBound);
+	for (int i = 1; i <= upperBound; i++)
+	{
+		if(sieve[i])
+		{
+			PrimeNumbers.insert(i);
+		}
+	}
+	cout << PrimeNumbers.size() << endl;
+	return PrimeNumbers;
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		cout << "Парметры ввода: <name.exe> <number>" << endl;
+		return 1;
+	}
+	int upperBound = atoi(argv[1]);
+	if (upperBound > MAX_UPPER_BOUND)
+	{
+		return 1;
+	}
+	GeneratePrimeNumbersSet(upperBound);
+	return 0;
+}
+
