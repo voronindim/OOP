4c56b77928b969de8817d377cc37c52466da5631
voronindim <51754697+voronindim@users.noreply.github.com>
2020-05-12 22:56:37 +0300

    add 5 CRational

---
+++ lab_5/Variant_3/CRational.cpp
@@ -0,0 +1,205 @@
+#include "CRational.h"
+#include <tuple>
+
+CRational::CRational()
+{
+	this->m_numerator = 0;
+	this->m_denominator = 1;
+}
+
+CRational::CRational(int value)
+{
+	this->m_numerator = value;
+	this->m_denominator = 1;
+}
+
+CRational::CRational(int numerator, int denominator)
+{
+	if (denominator == 0)
+	{
+		throw std::runtime_error("Знаменатель не может быть равен 0");
+	}
+	this->m_numerator = numerator;
+	this->m_denominator = denominator;
+	ReductionOfFraction();
+}
+
+int FindGreatestCommandDivisor(int num1, int num2)
+{
+//	numerator = abs(numerator);
+//	denominator = abs(denominator);
+	while (num2 != 0)
+	{
+		num1 %= num2;
+		std::tie(num1, num2);
+	}
+	return num1;
+}
+
+void CRational::ReductionOfFraction()
+{
+	int greatestCommandDivisor = FindGreatestCommandDivisor(m_numerator, m_denominator);
+	m_numerator /= greatestCommandDivisor;
+	m_denominator /= greatestCommandDivisor;
+	if (m_denominator < 0)
+	{
+		m_numerator = -m_numerator;
+		m_denominator = abs(m_denominator);
+	}
+}
+
+int CRational::GetNumerator() const
+{
+	return m_numerator;
+}
+
+int CRational::GetDenominator() const
+{
+	return m_denominator;
+}
+
+double CRational::ToDouble() const
+{
+	return ((double)m_numerator / (double)m_denominator);
+}
+
+CRational CRational::operator+() const
+{
+	return {m_numerator, m_denominator};
+}
+
+CRational CRational::operator-() const
+{
+	return {-m_numerator, m_denominator};
+}
+
+CRational& CRational::operator+=(const CRational& rational)
+{
+	m_numerator = rational.GetDenominator() * m_numerator + rational.GetNumerator() * m_denominator;
+	m_denominator *= rational.GetDenominator();
+	ReductionOfFraction();
+	return *this;
+}
+
+CRational& CRational::operator-=(const CRational& rational)
+{
+	m_numerator = rational.GetDenominator() * m_numerator - rational.GetNumerator() * m_denominator;
+	m_denominator *= rational.GetDenominator();
+	ReductionOfFraction();
+	return *this;
+}
+
+CRational& CRational::operator*=(const CRational& rational)
+{
+	m_numerator *= rational.GetNumerator();
+	m_denominator *= rational.GetDenominator();
+	ReductionOfFraction();
+	return *this;
+}
+CRational& CRational::operator/=(const CRational& rational)
+{
+	if (rational.GetNumerator() == 0)
+	{
+		throw std::runtime_error("Деление на 0");
+	};
+	m_numerator *= rational.GetDenominator();
+	m_denominator *= rational.GetNumerator();
+	ReductionOfFraction();
+	return *this;
+}
+std::pair<int, CRational> CRational::ToCompoundFraction() const
+{
+	int integerPart = m_numerator / m_denominator;
+	int numerator = m_numerator % m_denominator;
+	return std::make_pair(integerPart, {numerator, m_denominator});
+}
+
+bool operator==(const CRational& rational, const CRational& rational1)
+{
+	return (rational.GetNumerator() == rational1.GetNumerator() && rational.GetDenominator() == rational1.GetDenominator());
+}
+
+bool operator!=(const CRational& rational, const CRational& rational1)
+{
+	return !(rational == rational1);
+}
+
+bool operator<(const CRational& rational, const CRational& rational1)
+{
+	int num = rational.GetNumerator() * rational1.GetDenominator();
+	int num1 = rational1.GetNumerator() * rational.GetDenominator();
+	return num < num1;
+}
+
+bool operator>(const CRational& rational, const CRational& rational1)
+{
+	int num = rational.GetNumerator() * rational1.GetDenominator();
+	int num1 = rational1.GetNumerator() * rational.GetDenominator();
+	return num > num1;
+}
+
+bool operator<=(const CRational& rational, const CRational& rational1)
+{
+	int num = rational.GetNumerator() * rational1.GetDenominator();
+	int num1 = rational1.GetNumerator() * rational.GetDenominator();
+	return num <= num1;
+}
+
+bool operator>=(const CRational& rational, const CRational& rational1)
+{
+	int num = rational.GetNumerator() * rational1.GetDenominator();
+	int num1 = rational1.GetNumerator() * rational.GetDenominator();
+	return num >= num1;
+}
+
+CRational operator+(const CRational& rational, const CRational& rational1)
+{
+	int resultNumerator = rational.GetNumerator() * rational1.GetDenominator() + rational1.GetNumerator() * rational.GetDenominator();
+	int resultDenominator = rational.GetDenominator() * rational1.GetDenominator();
+	return {resultNumerator, resultDenominator};
+}
+
+CRational operator-(const CRational& rational, const CRational& rational1)
+{
+	int resultNumerator = rational.GetNumerator() * rational1.GetDenominator() - rational1.GetNumerator() * rational.GetDenominator();
+	int resultDenominator = rational.GetDenominator() * rational1.GetDenominator();
+	return {resultNumerator, resultDenominator};
+}
+
+CRational operator*(const CRational& rational, const CRational& rational1)
+{
+	int resultNumerator = rational.GetNumerator() * rational1.GetNumerator();
+	int resultDenominator = rational.GetDenominator() * rational1.GetDenominator();
+	return {resultNumerator, resultDenominator};
+}
+
+CRational operator/(const CRational& rational, const CRational& rational1)
+{
+	if (rational1.GetNumerator() == 0)
+	{
+		throw std::runtime_error("Деление на 0");
+	}
+	int resultNumerator = rational.GetNumerator() * rational1.GetDenominator();
+	int resultDenominator = rational.GetDenominator() * rational1.GetNumerator();
+	return {resultNumerator, resultDenominator};
+}
+
+std::ostream& operator<<(std::ostream& ostream, const CRational& rational)
+{
+	ostream << rational.GetNumerator() << '/' << rational.GetDenominator();
+	return ostream;
+}
+
+std::istream& operator>>(std::istream& istream, CRational& rational)
+{
+	int numerator, denominator;
+	if ((istream >> numerator) && (istream.get() == '/') && (istream >> denominator))
+	{
+		rational = CRational(numerator, denominator);
+	}
+	else
+	{
+		istream.setstate(std::ios_base::failbit | istream.rdstate());
+	}
+	return istream;
+}
---
+++ lab_5/Variant_3/CRational.h
@@ -0,0 +1,47 @@
+#pragma once
+#include <iostream>
+#include <utility>
+
+class CRational
+{
+public:
+	CRational();
+	explicit CRational(int value);
+	CRational(int numerator, int denominator);
+
+	int GetNumerator() const;
+	int GetDenominator() const;
+
+	double ToDouble() const;
+
+	CRational operator+() const;
+	CRational operator-() const;
+
+	CRational& operator+=(const CRational& rational);
+	CRational& operator-=(const CRational& rational);
+	CRational& operator*=(const CRational& rational);
+	CRational& operator/=(const CRational& rational);
+
+	std::pair<int, CRational> ToCompoundFraction() const;
+private:
+
+	void ReductionOfFraction();
+	int m_numerator;
+	int m_denominator;
+
+};
+
+CRational operator+(const CRational& rational, const CRational& rational1);
+CRational operator-(const CRational& rational, const CRational& rational1);
+CRational operator*(const CRational& rational, const CRational& rational1);
+CRational operator/(const CRational& rational, const CRational& rational1);
+
+bool operator==(const CRational& rational, const CRational& rational1);
+bool operator!=(const CRational& rational, const CRational& rational1);
+bool operator<(const CRational& rational, const CRational& rational1);
+bool operator>(const CRational& rational, const CRational& rational1);
+bool operator<=(const CRational& rational, const CRational& rational1);
+bool operator>=(const CRational& rational, const CRational& rational1);
+
+std::ostream& operator<<(std::ostream& ostream, const CRational& rational);
+std::istream& operator>>(std::istream& istream, CRational& rational);
